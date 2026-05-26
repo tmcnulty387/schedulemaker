edde16c97aa5985dfe1d7719d435bb709c92f2ee
tmcnulty387 <tmcnulty387@gmail.com>
2026-05-26 11:41:40 -0400

    Removed duplicate primary key from schema

--- schema/tables/schools.sql
+++ schema/tables/schools.sql
@@ -11,8 +11,7 @@ CREATE TABLE IF NOT EXISTS `schools` (
   `id`      INT UNSIGNED NOT NULL PRIMARY KEY,
   `number`  tinyint(2) UNSIGNED ZEROFILL NULL DEFAULT NULL,
   `code`    VARCHAR(5) NULL DEFAULT NULL,
-  `title` varchar(30) NOT NULL,
-  PRIMARY KEY  (`id`)
+  `title` varchar(30) NOT NULL
 ) ENGINE=InnoDb;
 
 -- ADD INDEXES -------------------------------------------------------------
